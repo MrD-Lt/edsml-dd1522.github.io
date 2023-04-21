57f2b70e123b5cce504a11513eb8b5a1c2f5fd33
Leslie Ting <leslie.ting.ddz@gmail.com>
2023-04-21 19:20:08 +0100

    bugs fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*!
+/*!/Users/leslieting/Documents/GitHub/MyStudyFolder/edsml-dd1522.github.io/js/scripts.js
 * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
